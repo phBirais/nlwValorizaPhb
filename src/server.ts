51b0e021241f54922430f84b8577997f0144c583
Pedro H Birais <phbirais@gmail.com>
2021-06-20 22:57:06 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { request, response } from "express";
+//@types/express
+const app = express();
+
+/*
+GET    => Buscar uma informação 
+POST   => Inserir (Criar) uma informação 
+PUT    => Alterar uma informação
+DELETE => Remover um dado
+PATCH  => Alterar uma informação específica 
+*/
+app.get("/test", (request, response) => {
+  //Request => entrando
+  //Response => saindo
+  return response.send("Olá NLW");
+});
+
+app.post("/test-post", (request, response) => {
+  return response.send("Olá NLW Método Post");
+});
+
+// http://localhost:3000
+app.listen(3000, () => console.log("Server is running"))
